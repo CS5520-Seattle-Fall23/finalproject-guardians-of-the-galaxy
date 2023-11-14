ff19a70662ff1af514ba720c6aec7e35ac4c6117
zhen <sallyzzbb@163.com>
2023-11-13 18:55:02 -0800

    update sleepRecordScreen

---
+++ posilife_1/lib/sleepRecordScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class SleepRecordScreen extends StatefulWidget {
+  @override
+  _SleepRecordScreen createState() => _SleepRecordScreen();
+}
+class _SleepRecordScreen extends State<SleepRecordScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    );
+}
+}
