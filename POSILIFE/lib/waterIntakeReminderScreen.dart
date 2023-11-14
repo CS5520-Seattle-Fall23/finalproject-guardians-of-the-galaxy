d168791eb8ae5f883b2376a635f77b1ff51e9bef
JonathanZha47 <zhayiwei@bu.edu>
2023-11-13 17:37:33 -0800

    Frontend Features
    - authentication
    - homescreen
    - waterintakescreen(still work on it)

---
+++ POSILIFE/lib/waterIntakeReminderScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class WaterIntakeReminderScreen extends StatefulWidget {
+  @override
+  _WaterIntakeReminderScreenState createState() => _WaterIntakeReminderScreenState();
+}
+class _WaterIntakeReminderScreenState extends State<WaterIntakeReminderScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    );
+}
+}
